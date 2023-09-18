2378a2fcf78b094ce11735d7f39eae9c0392d47b
Shantanu993 <shantanugahtori4@gmail.com>
2023-09-18 21:12:36 +0530

    added navbar

--- my-app/components/Navbar/index.jsx
+++ my-app/components/Navbar/index.jsx
@@ -3,7 +3,58 @@ import React from "react";
 const Navbar = () => {
   return (
     <>
-      
+      <nav id="desktop-nav">
+        <div class="logo">Shantanu Gahtori</div>
+        <div>
+          <ul class="nav-links">
+            <li>
+              <a href="#about">About</a>
+            </li>
+            <li>
+              <a href="#skills">Skills</a>
+            </li>
+            <li>
+              <a href="#projects">Projects</a>
+            </li>
+            <li>
+              <a href="#contact">Contact</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+      <nav id="hamburger-nav">
+        <div class="logo">Shantanu Gahtori</div>
+        <div class="hamburger-menu">
+          <div class="hamburger-icon" onclick="toggleMenu()">
+            <span></span>
+            <span></span>
+            <span></span>
+          </div>
+          <div class="menu-links">
+            <li>
+              <a href="#about" onclick="toggleMenu()">
+                About
+              </a>
+            </li>
+            <li>
+              <a href="#skills" onclick="toggleMenu()">
+                Skills
+              </a>
+            </li>
+            <li>
+              <a href="#projects" onclick="toggleMenu()">
+                Projects
+              </a>
+            </li>
+            <li>
+              <a href="#contact" onclick="toggleMenu()">
+                Contact
+              </a>
+            </li>
+          </div>
+        </div>
+      </nav>
+    </>
   );
 };
 
