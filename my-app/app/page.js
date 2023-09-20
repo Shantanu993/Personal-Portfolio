7295e4553d551ac87fff9783b37e1ef5386ffb3a
Shantanu993 <shantanugahtori4@gmail.com>
2023-09-21 00:29:50 +0530

    updated homepage

--- my-app/app/page.js
+++ my-app/app/page.js
@@ -1,10 +1,14 @@
+import About from '@/components/About'
 import Navbar from '@/components/Navbar'
+import Profile from '@/components/Profile'
 import Image from 'next/image'
 
 export default function Home() {
   return (
     <>
       <Navbar />
+      <Profile/>
+      <About/>
     </>
   )
 }
